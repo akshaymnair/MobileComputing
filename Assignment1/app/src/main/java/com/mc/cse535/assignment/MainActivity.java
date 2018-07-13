823950cb4b22cfa52ee405522f1594570089852b
Akshay <akshayms@asu.edu>
2018-07-13 02:23:00 -0700

    Feat: Added button actions for Run and Stop, Assignment 1 tasks completed. Testing pending

--- Assignment1/app/src/main/java/com/mc/cse535/assignment/MainActivity.java
+++ Assignment1/app/src/main/java/com/mc/cse535/assignment/MainActivity.java
@@ -1,32 +1,67 @@
 package com.mc.cse535.assignment;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private Handler graphHandler = new Handler();
+    // the message queue to communicate to runGraph thread
+    private double lastXpoint=0;
+    private Random random = new Random();
+    private  Runnable graphThread;
+
+    GraphView graph;
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GraphView graph =  findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(getDatapoint());
-        graph.addSeries(series);
+        graph = findViewById(R.id.graph);
+        series = new LineGraphSeries<>();
+
+        final Button runButton = findViewById(R.id.runButton);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                graph.addSeries(series);
+                runGraph();
+            }
+        });
+
+        final Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                stopGraph();
+            }
+        });
     }
-        private DataPoint[] getDatapoint () {
-            DataPoint[] dp = new DataPoint[]{
-                    new DataPoint(0, 1),
-                    new DataPoint(1, 5),
-                    new DataPoint(2, 3),
-                    new DataPoint(3, 2),
-                    new DataPoint(4, 6)
-            };
-            return dp;
-        }
+    private void stopGraph(){
+        graph.removeAllSeries();
+        graphHandler.removeCallbacks(graphThread);
+    }
+    private void runGraph() {
+        graphThread = new Runnable() {
+            @Override
+            public void run() {
+                series.appendData(new DataPoint(lastXpoint,random.nextInt(500)),false,1000);
+                lastXpoint+=50;
+//                graph.addSeries(series);
+                runGraph();
+            }
+        };
+        graphHandler.postDelayed(graphThread,500);
+    }
 }
 
